5802e32ef99629c01a859f081cd151f9ad748b42
Shuvam Mishra <shuvamishra1920@gmail.com>
2022-01-19 20:35:50 +0545

    final commit

--- assets/Analog-clock.js
+++ assets/Analog-clock.js
@@ -1,7 +1,10 @@
-function datFunction(){
+setInterval(digitalFunction, 1000);
+setInterval(analogFunction, 1000);
+
+function analogFunction(){
     let source = new Date();
     let AmPm = source.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }) 
-    let AmPmVal = AmPm.slice(7,10);
+    let AmPmVal = AmPm.slice(8,10);
     let AmPmDiv = document.querySelector('.AmPm').innerHTML = AmPmVal;
     let minutes = source.getMinutes();
     let hours = source.getHours(); 
@@ -18,7 +21,7 @@ function datFunction(){
     }
 
     let secondHandDeg = seconds * 6;
-    let minuteHandDeg = minutes * 6;
+    let minuteHandDeg = (minutes * 6) + (seconds * 0.1);
     let hourHandDeg = (hours * 30) + (minutes * 0.5);
     // console.log(seconds)
     // console.log(minutes)   
@@ -32,6 +35,37 @@ function datFunction(){
 
     let hourHand = document.querySelector('.hour-hand');
     hourHand.style.transform = `rotate(${hourHandDeg}deg)`;
-} 
+}
 
-setInterval(datFunction, 1000)
+function digitalFunction(){
+    const samayaDiv = document.querySelector('.time');
+    const ghanta = document.querySelector('.hours'); 
+    const minute = document.querySelector('.minute'); 
+    const second = document.querySelector('.second'); 
+    const amPm = document.querySelector('.amPm'); 
+    function myFunction(){
+        var time = new Date();
+          var timeValue = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }) 
+          var ghantaValue = timeValue.slice(0, 2);
+          var minuteValue = timeValue.slice(3, 5);
+          var secondValue = timeValue.slice(6, 8);
+          var amPmValue = timeValue.slice(9, 12);
+      
+        var splitHour = timeValue.slice(1, 2);
+        if(splitHour === ":") {
+          ghantaValue = timeValue.slice(0, 1);
+          minuteValue = timeValue.slice(2, 4);
+          secondValue = timeValue.slice(5, 7);
+          amPmValue = timeValue.slice(8, 11)
+        }
+        ghanta.innerHTML = ghantaValue;
+        minute.innerHTML = minuteValue;
+        second.innerHTML = secondValue;
+        amPm.innerHTML = amPmValue;
+      }
+      setInterval(myFunction, 1000)
+}
+
+analogFunction();
+
+digitalFunction();
